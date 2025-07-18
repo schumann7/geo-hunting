ec8c78809c38be59ff7ee342755de1bd5357ac08
unknown <romulonilsonhorn@gmail.com>
2025-07-18 08:47:48 -0300

    lógica para pegar as salas e mostrar ao usuário

---
+++ lib/logic/find_rooms.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List> findRooms() async {
+  final response = await http.Client().get(
+    Uri.parse('http://192.168.86.212:5000/find_rooms'),
+  );
+  debugPrint(response.toString());
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    print(data);
+    return data;
+  } else {
+    print('Erro: ${response.statusCode}');
+  }
+  return ['Erro'];
+}
